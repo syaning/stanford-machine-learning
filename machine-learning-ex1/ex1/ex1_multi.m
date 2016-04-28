4e8be5b44c7d0149c2147ad364f6507060089f5d
syaning <syaningv@gmail.com>
2016-04-28 14:58:07 +0800

    accomplish ex1

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,8 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+% price = 0; % You should change this
+price = [1 ((1650 - mu(1)) / sigma(1)) ((3 - mu(2)) / sigma(2))] * theta;
 
 % ============================================================
 
@@ -149,8 +149,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+% price = 0; % You should change this
+price = [1 1650 3] * theta;
 
 % ============================================================
 
