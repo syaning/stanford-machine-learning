b85849802fbf8754c1534c17acb2487769d22006
syaning <syaningv@gmail.com>
2016-12-05 16:44:00 +0800

    ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+h = sigmoid(X * theta);
+J = (1 / m) * sum(-y .* log(h) - (1 .- y) .* log(1 .- h)) + lambda / 2 / m * (theta(2:end)' * theta(2:end));
+grad = 1 / m * X' * (h .- y);
+temp = theta;
+temp(1) = 0;
+grad = grad + (lambda / m) .* temp;
 
 % =============================================================
 
