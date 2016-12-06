b637bf43e1acdaade82da9b56ca11ddf40f7198f
syaning <syaningv@gmail.com>
2016-12-06 13:59:15 +0800

    ex3

--- machine-learning-ex3/ex3/lib/submitWithConfiguration.m
+++ machine-learning-ex3/ex3/lib/submitWithConfiguration.m
@@ -63,7 +63,8 @@ function response = submitParts(conf, email, token, parts)
   body = makePostBody(conf, email, token, parts);
   submissionUrl = submissionUrl();
   params = {'jsonBody', body};
-  responseBody = urlread(submissionUrl, 'post', params);
+  % responseBody = urlread(submissionUrl, 'post', params);
+  [code, responseBody] = system(sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, submissionUrl));
   response = loadjson(responseBody);
 end
 
