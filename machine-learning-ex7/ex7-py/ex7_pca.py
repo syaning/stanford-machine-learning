d5a4d3c1dd57eb42473d3fc63368ef92f5c10820
syaning <syaningv@gmail.com>
2016-12-21 13:56:42 +0800

    update

---
+++ machine-learning-ex7/ex7-py/ex7_pca.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.io import loadmat
+
+
+# ================== Part 1: Load Example Dataset  ===================
+#  We start this exercise by using a small dataset that is easily to
+#  visualize
